46f7e23b4f7322fc22a1d71e575a53a4c9918a68
Michael Alabi <alabimichael23@gmail.com>
2022-10-07 12:27:04 +0100

    Python Concatenation Task

---
+++ PycharmProjects/pythonProject/main.py
@@ -0,0 +1,7 @@
+# Task 3 Module 3B
+
+gender = "Mr"
+name = " Michael"
+biodata = gender + name
+
+print(biodata)
---
+++ PycharmProjects/pythonProject/mainmain.py
@@ -0,0 +1,74 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#integer
+
+number1 = 12345
+number2 = -12345
+long_number = 112223444546738339393939330003003003
+
+# print(type(number1))      # <....... int>
+# print(type(number2))      # <....... int>
+# print(type(long_number))  # <....... int>
+#
+# name = 10          # int
+# print(type(name))  # <... int>
+#
+# name = 'testify'   # str
+# print(type(name))  # <... str>
+#
+# name = False       # bool
+# print(type(name))  # <... bool>
+
+#Task 1
+pythonTask = 'hello world'
+# print(pythonTask)
+
+#name = 'Michael Alabi'
+#print(name)
+
+
+#Task 2
+#
+# variable_name = 'my_var'    # str
+# print(type(variable_name))  # <... str>
+#
+# variable_name = 12.6        # int
+# print(type(variable_name))  # <... int>
+#
+# int_variable = 200          # int
+# print(type(int_variable))   # <... int>
+#
+#
+# name = 'Michael Alabi'      # int
+# print(name)                 # <... int>
+
+name = "Testify"
+lesson = 'Python, String and Concatenation'
+
+# escape sequence
+
+article = "This is an article\na multiline article\n\tEach text.\b"
+print(article)
+
+# concatenation
+group = "wood"
+attr = "peckers"
+bird = group + attr
+
+print(bird)
+print("Python " + "Programming" + " Language")
